ecb8ae74c9069e6cdaeb8ee3f6fba3baff6d57c8
Projaro <services@projaro.com>
2020-11-19 20:40:06 +0100

    Changes by Mrs Omotola

--- assets/js/script.js
+++ assets/js/script.js
@@ -414,3 +414,20 @@ $(document).ready(function() {
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 });
+/*
+=============================================== 10. Add new field ===============================================
+*/
+
+function add(){
+    var new_chq_no = parseInt($('#total_chq').val())+1;
+    var new_input="<input type='text' class='form-control' id='new_"+new_chq_no+"' placeholder='Type email here...'>";
+    $('#new_chq').append(new_input);
+    $('#total_chq').val(new_chq_no)
+  }
+  function remove(){
+    var last_chq_no = $('#total_chq').val();
+    if(last_chq_no>1){
+      $('#new_'+last_chq_no).remove();
+      $('#total_chq').val(last_chq_no-1);
+    }
+  }
